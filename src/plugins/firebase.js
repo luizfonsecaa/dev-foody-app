6576b462e5af71fce484514eeecdf33c5cbf5a9a
Evellyn <xe.uyemura@gmail.com>
2020-07-27 22:20:32 -0300

    first commit

---
+++ src/plugins/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBwJQ8yW4NXYzT4rx8fyZ4t0t9GW6fNAX4",
+    authDomain: "dev-foody.firebaseapp.com",
+    databaseURL: "https://dev-foody.firebaseio.com",
+    projectId: "dev-foody",
+    storageBucket: "dev-foody.appspot.com",
+    messagingSenderId: "623400202242",
+    appId: "1:623400202242:web:4be2756c52134cb0f5b71b"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
+
